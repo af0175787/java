df562a48e72a42a742953333c9a5e9f713c9c7d9
KeerthikaSekar <103176886+af0175787@users.noreply.github.com>
2022-04-08 23:54:04 -0700

    Add files via upload

---
+++ Practical/Ifelseexp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Ifelseexp
+{
+public void display()
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter age");
+int age=s.nextInt();
+if(age>=18)
+{
+system.out.println("your are eligible");
+}
+else
+{
+System.out.println("your are not eligible");
+}
+}
+public static void main(String args[])
+{
+Ifelseexp obj=newifelseexp();
+obj.display();
+}
+}
