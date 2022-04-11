e954d76cdd8d31a0c70a653d1b94878575e5aadf
KeerthikaSekar <103176886+af0175787@users.noreply.github.com>
2022-04-10 20:53:55 -0700

    Add files via upload

---
+++ Assignment/Factorial.java
@@ -0,0 +1,17 @@
+public class Factorial{
+     public static void main(String args[]){
+         int num=5;
+         long factorial=1;
+
+         for(int i=1; i<=num; i++){
+           System.out.print(i+" * ");
+           }
+
+           System.out.println();
+
+         for(int i=1; i<=num; i++){
+
+           System.out.println(factorial*=i);
+}
+}
+}
